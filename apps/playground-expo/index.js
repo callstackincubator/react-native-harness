6f1b61f7b0733750bdb2038580d55a54bc0431d1
Szymon Chmal <szymon@chmal.it>
2025-09-23 10:32:28 +0200

    feat: remove expo example

--- apps/playground-expo/index.js
+++
@@ -1,8 +0,0 @@
-import { registerRootComponent } from 'expo';
-import { getEntryComponent, UI } from '@react-native-harness/runtime';
-import App from './src/app/App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(getEntryComponent(UI));
