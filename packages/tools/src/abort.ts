6c38e4868bbcf4fff0f45de709d06c368f348a58
Szymon Chmal <szymon@chmal.it>
2026-03-30 15:00:29 +0200

    Use native AbortSignal helpers

--- packages/tools/src/abort.ts
+++ packages/tools/src/abort.ts
@@ -1,56 +1,12 @@
-const createAbortError = () =>
-  new DOMException('The operation was aborted', 'AbortError');
-
 export const getTimeoutSignal = (timeout: number): AbortSignal => {
-  const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(createAbortError()), timeout);
-  timeoutId.unref?.();
-
-  controller.signal.addEventListener(
-    'abort',
-    () => {
-      clearTimeout(timeoutId);
-    },
-    { once: true }
-  );
-
-  return controller.signal;
+  return AbortSignal.timeout(timeout);
 };
 
 export const raceAbortSignals = (signals: AbortSignal[]): AbortSignal => {
-  const controller = new AbortController();
-
   if (signals.length === 0) {
-    return controller.signal;
+    return new AbortController().signal;
   }
-
-  const cleanupCallbacks: Array<() => void> = [];
-
-  const cleanup = () => {
-    while (cleanupCallbacks.length > 0) {
-      cleanupCallbacks.pop()?.();
-    }
-  };
-
-  const abortWithSignal = (signal: AbortSignal) => {
-    cleanup();
-    controller.abort(signal.reason ?? createAbortError());
-  };
-
-  for (const signal of signals) {
-    if (signal.aborted) {
-      abortWithSignal(signal);
-      return controller.signal;
-    }
-
-    const onAbort = () => abortWithSignal(signal);
-    signal.addEventListener('abort', onAbort, { once: true });
-    cleanupCallbacks.push(() => signal.removeEventListener('abort', onAbort));
-  }
-
-  controller.signal.addEventListener('abort', cleanup, { once: true });
-
-  return controller.signal;
+  return AbortSignal.any(signals);
 };
 
 export const withAbortTimeout = (
